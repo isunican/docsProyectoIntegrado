cea1e3ae7437bb2aa941452c7e2de77ef79f3106
Pablo Sánchez <sanchezbp@unican.es>
2023-10-24 20:18:13 +0200

    Add theme folder

--- conf.py
+++ conf.py
@@ -80,6 +80,8 @@ pygments_style = None
 # html_theme = 'alabaster'
 html_theme = "sphinx_rtd_theme"
 
+html_theme_path = ["_themes", ]
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
