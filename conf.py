9df3c1159a74351b15c327eb84cc2b1b5732de04
Pablo Sánchez <p.sanchez@unican.es>
2023-10-24 13:42:22 +0200

    Import RTD

--- conf.py
+++ conf.py
@@ -15,7 +15,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-
+import sphinx-rtd-theme
 
 # -- Project information -----------------------------------------------------
 
